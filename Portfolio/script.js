115521b063d12fa111421e74b517ea8997c33ab5
MuskanPriyaSingh <muskanpriya610@gmail.com>
2023-08-30 23:57:32 +0530

    Completed portfolio task.

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+/*   typing animation    */
+var typed = new Typed(".typing",{
+    strings:["","Frontend Developer","Web Designer","web Developer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
